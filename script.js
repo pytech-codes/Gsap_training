dd4c43f287f963d4634186e941e8253642b64b94
pytech-codes <pytech_goals@gmail.com>
2025-11-14 20:14:12 -0400

    Implemented The Footer Page

--- script.js
+++ script.js
@@ -160,3 +160,24 @@ const maskTime = gsap.timeline({
   ease:'power1.inOut'
   
 })
+
+const titlSplit = SplitText.create("#contact h2", {type:'words'});
+const timelines = gsap.timeline({
+  scrollTrigger:{
+    trigger:"#contact",
+    start:'top center',   
+  },
+  ease:'power1.inOut'
+})
+timelines.from(
+  titlSplit.words,{
+    opacity:0, yPercent:100, stagger:0.02
+  }
+).from('#contact h3, #contact p',{
+  opacity:0, yPercent:100, stagger:0.02
+}).to("#f-right-leaf",{
+  y:'-50', duration:1,ease:'power1.inOut'
+})
+.to("#f-left-leaf",{
+  y:'-50', duration:1,ease:'power1.inOut'
+}, '<')
